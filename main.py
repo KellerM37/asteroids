72a7c75ca2f25407ecdbf5d9f994c8def590d2a8
KellerM37 <127287343+KellerM37@users.noreply.github.com>
2024-08-21 14:35:00 -0500

    We now open and display a plain black window. But it's a window

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	
+	pygame.init()
+	inf = True
 
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	while inf == True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill((0,0,0))
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
