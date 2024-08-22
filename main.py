fa66d644e195ad9790e6745507dcab8d5bce01a9
KellerM37 <127287343+KellerM37@users.noreply.github.com>
2024-08-22 10:51:37 -0500

    added asteroids, shooting, and losing

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (updatable, drawable, asteroids)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroidField = AsteroidField()
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+
+		for x in updatable:
+			x.update(dt)
+
+		for x in asteroids:
+			if x.collision(player) == True:
+				print("Game over!")
+				exit()
+
 		screen.fill(("black"))
-		player.update(dt)
-		player.draw(screen)
+
+		for x in drawable:
+			x.draw(screen)
+
 		pygame.display.flip()
 
 		# limit framerate to 60 FPS
